cc39adacc2776a562a4892897160b6966f1bbfd0
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-22 13:17:48 -0500

    Initial commit

---
+++ src/main/java/com/restaurant_ddd/RestaurantDddApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant_ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantDddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restaurant_ddd/RestaurantDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant_ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
